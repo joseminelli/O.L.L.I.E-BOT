de5efb27103d173f3413b13e1e63b659bf920bf6
José Minelli <54222151+joseminelli@users.noreply.github.com>
2023-10-18 12:07:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -150,4 +150,4 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-client.login("MTE0MzE5OTkxNDM4MjMyNzgyOA.Gp_cd-.PTfzNtcCOPQ10WHQc-f7fvtMg2CbJsmuLMtXRw");
+client.login("SUA KEY");
